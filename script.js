9ddd2bf8a59d8721cf0ae3ff1fe0ef5a53ef2257
Abreham <abrehamodin26@gmail.com>
2024-02-23 03:59:41 +0300

    Added a function to play one round game

--- script.js
+++ script.js
@@ -5,3 +5,46 @@ function getComputerChoice() {
     return items[Math.floor(Math.random() * items.length)];
 }
 
+let userCounter = 0;
+let computerCounter = 0;
+
+// enable to play one round of rock-paper-scissor game.
+function playRound(playerChoice, computerChoice) {
+    
+    if(playerChoice == "SCISSOR") {
+        if(computerChoice == "PAPER") {
+            userCounter++;
+            return "You Win! Scissor beats Paper.";
+        }
+        else if(computerChoice == "ROCK") {
+            computerCounter++;
+            return "You Lose! Rock beats Scissor."; 
+        }
+        else if(computerChoice == "SCISSOR")
+            return "It's Tie!";
+    }
+    else if(playerChoice == "PAPER") {
+        if(computerChoice == "ROCK") {
+            userCounter++;
+            return "You Win! Paper beats Rock."; 
+        }
+        else if(computerChoice == "SCISSOR") {
+            computerCounter++;
+            return "You Lose! Scissor beats Paper."; 
+        }
+        else if(computerChoice == "PAPER")
+            return "It's Tie!";
+    }
+    else if(playerChoice == "ROCK") {
+        if(computerChoice == "Scissor") {
+            userCounter++;
+            return "You Win! Rock beats Scissor.";
+        }
+        else if(computerChoice == "PAPER") {
+            computerCounter++;
+            return "You Lose! Paper beats Rock.";
+        }
+        else if(computerChoice == "ROCK")
+            return "It's Tie!";
+    }
+}
